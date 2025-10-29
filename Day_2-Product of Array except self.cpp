36fe743f6e4d42a05d311376b4042fc8119a5d66
Try2Hard <77232305+Pradipta190102@users.noreply.github.com>
2025-10-30 01:45:02 +0530

    Create Day_2-Product of Array except self.cpp

---
+++ Day_2-Product of Array except self.cpp
@@ -0,0 +1,24 @@
+//Leetcode solution
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+         
+         int n=nums.size();
+       vector<int>ans(n);
+       int prefix=1;
+       for(int i=0;i<n;i++)
+       {
+          ans[i]=prefix;
+          prefix=nums[i]*prefix;
+       }
+       int postfix=1;
+       for(int i=n-1;i>=0;i--)
+       {
+          ans[i]=ans[i]*postfix;
+          postfix=nums[i]*postfix;
+       }
+        return ans;
+
+    }
+};
